540075e0c14c6166a726d70597d77b12e3076915
thesantosh <santosh.venk@gmail.com>
2014-09-19 01:16:26 +0530

    Add jQuery,correctly formatted HTML Table with spurious data

--- routes.py
+++ routes.py
@@ -6,7 +6,16 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
-bunk_data = {'subject':'Soft Computing Techniques','bunks':12,'present':29}
+bunk_data = [
+{'subject':'Soft Computing Techniques','bunks':12,'present':29},
+{'subject':'Cloud Computing','bunks':9,'present':36},
+{'subject':'Middleware Technologies','bunks':14,'present':27},
+{'subject':'Mobile Computing','bunks':14,'present':26},
+{'subject':'Middleware Technologies Lab','bunks':6,'present':21},
+{'subject':'Mobile Computing Lab','bunks':7,'present':23},
+{'subject':'Embedded Systems','bunks':10,'present':43},
+{'subject':'Green Technology','bunks':12,'present':34}
+]
 
 @app.route('/bunkbazaar')
 def bunkbazaar():
---
+++ static/js/application.js
@@ -0,0 +1,7 @@
+$(document).ready(function()
+	{ 
+		$('li').click(function()
+			{
+				$(this).toggleClass('active');
+			});
+	});
